5724c4a0c0b80bf7328c8d5abd342abab771e876
kacperpucylo <pucylek@wp.pl>
2023-09-24 22:41:18 +0200

    player movement, basic camera and basic tilemap

---
+++ scenes/player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 200
+
+func _ready():
+	pass
+
+func _process(delta):
+	var direction = get_movement_vector().normalized()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+
+func get_movement_vector() -> Vector2:
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(x_movement, y_movement)
