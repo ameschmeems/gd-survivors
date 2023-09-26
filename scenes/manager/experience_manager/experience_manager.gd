976dcb23c130ede248fda5875ed9c19596bb58d9
kacperpucylo <pucylek@wp.pl>
2023-09-26 19:22:11 +0200

    ability upgrade ui + folder structure

--- scenes/manager/experience_manager/experience_manager.gd
+++ scenes/manager/experience_manager/experience_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 signal experience_updated(current_experience: float, target_experience: float)
-signal level_up
+signal level_up(level: int)
 
 const TARGET_EXP_GROWTH = 5
 
@@ -10,17 +10,17 @@ var current_level = 1
 var target_exp = 5
 
 func _ready():
-    GameEvents.exp_vial_collected.connect(on_exp_vial_collected)
+	GameEvents.exp_vial_collected.connect(on_exp_vial_collected)
 
 func increment_experience(number: float) -> void:
-    current_exp = min(current_exp + number, target_exp)
-    if current_exp == target_exp:
-        current_level += 1
-        target_exp += TARGET_EXP_GROWTH
-        current_exp = 0
-        level_up.emit()
-    experience_updated.emit(current_exp, target_exp)
-    # print(current_exp)
+	current_exp = min(current_exp + number, target_exp)
+	if current_exp == target_exp:
+		current_level += 1
+		target_exp += TARGET_EXP_GROWTH
+		current_exp = 0
+		level_up.emit(current_level)
+	experience_updated.emit(current_exp, target_exp)
+	# print(current_exp)
 
 func on_exp_vial_collected(number: float) -> void:
-    increment_experience(number)
+	increment_experience(number)
