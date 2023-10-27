06378195e6ad9fecd1fff8e092e80bf2e7546d48
kacperpucylo <pucylek@wp.pl>
2023-10-27 23:09:24 +0200

    arena walls and preventing out of bounds enemy spawn

--- scenes/manager/enemy_manager/enemy_manager.gd
+++ scenes/manager/enemy_manager/enemy_manager.gd
@@ -14,6 +14,26 @@ func _ready():
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
+func get_spawn_position() -> Vector2:
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if !player:
+		return Vector2.ZERO
+
+	var spawn_position = Vector2.ZERO
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	for i in 4:
+		spawn_position = player.global_position + random_direction * SPAWN_RADIUS
+
+		var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1 << 0)
+		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
+
+		if result.is_empty():
+			break
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+	
+	return spawn_position
+
 func on_timer_timeout():
 	# done this way so that changed wait time only applies after the timer runs out
 	timer.start()
@@ -21,14 +41,11 @@ func on_timer_timeout():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if !player:
 		return
-	
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position = player.global_position + random_direction * SPAWN_RADIUS
 
 	var enemy = basic_enemy_scene.instantiate() as Node2D
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position()
 
 func on_arena_difficulty_increased(arena_difficulty: int):
 	var time_off = (.1/12) * arena_difficulty
