388efc8d4171df32110669359adb1039e1555fce
kacperpucylo <pucylek@wp.pl>
2023-10-29 15:28:22 +0100

    animated exp pick up

--- scenes/game_object/exp_vial/exp_vial.gd
+++ scenes/game_object/exp_vial/exp_vial.gd
@@ -1,8 +1,32 @@
 extends Node2D
 
+@onready var collision_shape_2d = $Area2D/CollisionShape2D
+
 func _ready():
 	$Area2D.area_entered.connect(on_area_entered)
 
-func on_area_entered(_other_area: Area2D):
+func tween_collect(percent: float, start_position: Vector2):
+	var player = get_tree().get_first_node_in_group("player")
+	if !player:
+		return
+	
+	global_position = start_position.lerp(player.global_position, percent)
+	var direction_from_start = player.global_position - start_position
+
+	var target_rotation = direction_from_start.angle() + deg_to_rad(90)
+	rotation = lerp_angle(rotation, target_rotation, 1 - exp(-2 * get_process_delta_time()))
+
+func collect():
 	GameEvents.emit_exp_vial_collected(1)
 	queue_free()
+
+func disable_collision():
+	collision_shape_2d.disabled = true
+
+func on_area_entered(_other_area: Area2D):
+	Callable(disable_collision).call_deferred()
+	var tween = create_tween()
+	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, 1.0) \
+		.set_ease(Tween.EASE_IN) \
+		.set_trans(Tween.TRANS_BACK)
+	tween.tween_callback(collect)
