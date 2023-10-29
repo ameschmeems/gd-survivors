57cd2c6d365e03af4635eb7fa9665c518fa7186b
kacperpucylo <pucylek@wp.pl>
2023-10-29 16:16:15 +0100

    floating damage numbers

--- scenes/component/hurtbox_component/hurtbox_component.gd
+++ scenes/component/hurtbox_component/hurtbox_component.gd
@@ -3,15 +3,21 @@ class_name HurtboxComponent
 
 @export var health_component: HealthComponent
 
+var floating_text_scene = preload("res://scenes/ui/floating_text/floating_text.tscn")
+
 func _ready():
-    area_entered.connect(on_area_entered)
+	area_entered.connect(on_area_entered)
 
 func on_area_entered(other_area: Area2D) -> void:
-    if !(other_area is HitboxComponent):
-        return
-    if !health_component:
-        return
-    
-    var hitbox_component = other_area as HitboxComponent
-    health_component.damage(hitbox_component.damage)
-    
+	if !(other_area is HitboxComponent):
+		return
+	if !health_component:
+		return
+	
+	var hitbox_component = other_area as HitboxComponent
+	health_component.damage(hitbox_component.damage)
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+
+	floating_text.global_position = global_position
+	floating_text.start(str(hitbox_component.damage))
--- scenes/game_object/exp_vial/exp_vial.gd
+++ scenes/game_object/exp_vial/exp_vial.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var collision_shape_2d = $Area2D/CollisionShape2D
+@onready var sprite = $Sprite2D
 
 func _ready():
 	$Area2D.area_entered.connect(on_area_entered)
@@ -26,7 +27,11 @@ func disable_collision():
 func on_area_entered(_other_area: Area2D):
 	Callable(disable_collision).call_deferred()
 	var tween = create_tween()
-	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, 1.0) \
+	tween.set_parallel()
+	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, .5) \
 		.set_ease(Tween.EASE_IN) \
 		.set_trans(Tween.TRANS_BACK)
+	tween.tween_property(sprite, "scale", Vector2.ZERO, .05) \
+		.set_delay(.45)
+	tween.chain()
 	tween.tween_callback(collect)
