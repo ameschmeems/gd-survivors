a1de2ad49eea5f88c57185c87cde7ffbdfd40d3f
kacperpucylo <pucylek@wp.pl>
2023-10-31 00:06:24 +0100

    movement speed upgrade and more ui improvements

--- scenes/autoload/game_events/game_events.gd
+++ scenes/autoload/game_events/game_events.gd
@@ -2,9 +2,13 @@ extends Node
 
 signal exp_vial_collected(number: float)
 signal ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary)
+signal player_damaged
 
 func emit_exp_vial_collected(number: float) -> void:
-    exp_vial_collected.emit(number)
+	exp_vial_collected.emit(number)
 
 func emit_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
-    ability_upgrade_added.emit(upgrade, current_upgrades)
+	ability_upgrade_added.emit(upgrade, current_upgrades)
+
+func emit_player_damaged():
+	player_damaged.emit()
--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,18 +1,19 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 125
-const ACCELERATION_SMOOTHING = 25
-
 @onready var damage_interval_timer = $DamageIntervalTimer
 @onready var health_component = $HealthComponent
 @onready var health_bar = $HealthBar
 @onready var abilities = $Abilities
 @onready var animation_player = $AnimationPlayer
 @onready var visuals = $Visuals
+@onready var velocity_component = $VelocityComponent
 
 var number_colliding_bodies = 0
+var base_speed = 0
 
 func _ready():
+	base_speed = velocity_component.max_speed
+
 	$CollisionArea2D.body_entered.connect(on_body_entered)
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
@@ -22,9 +23,8 @@ func _ready():
 
 func _process(delta):
 	var direction = get_movement_vector().normalized()
-	var target_velocity = direction * MAX_SPEED
-	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
-	move_and_slide()
+	velocity_component.accelerate_in_direction(direction)
+	velocity_component.move(self)
 
 	if direction.x != 0 || direction.y != 0:
 		animation_player.play("walk")
@@ -60,11 +60,12 @@ func on_damage_interval_timer_timeout() -> void:
 	check_deal_damage()
 
 func on_health_changed() -> void:
+	GameEvents.emit_player_damaged()
 	update_health_display()
 
-func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, _current_upgrades: Dictionary):
-	if not (ability_upgrade is Ability):
-		return
-	
-	var ability = ability_upgrade as Ability
-	abilities.add_child(ability.ability_controller_scene.instantiate())
+func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if (ability_upgrade is Ability):
+		var ability = ability_upgrade as Ability
+		abilities.add_child(ability.ability_controller_scene.instantiate())
+	elif ability_upgrade.id == "player_speed":
+		velocity_component.max_speed = base_speed + (base_speed * current_upgrades["player_speed"]["quantity"] * .1)
